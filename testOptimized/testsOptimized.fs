c38a2653a6137ff0b453ab8047ed313bb7c79162
Okke Hendriks <okke.hendriks@prodrive-technologies.com>
2021-02-17 22:18:17 +0100

    Add slightly more complex example.

--- testOptimized/testsOptimized.fs
+++ testOptimized/testsOptimized.fs
@@ -1,7 +1,28 @@
 ﻿module test
 open FsCheck
+open FsCheck.Xunit
 
+/// Very simple example.
 [<Xunit.Fact>]
-let testOptimized () =
+let testOptimizedFact () =
     FsCheck.Prop.discard ()
     true
+
+
+
+/// Slightly more complex example.
+type OnlyTrue = OnlyTrue of bool
+
+let chooseTrueGen =
+    gen {
+        match! Gen.choose (0, 10) with
+        | 0 -> return OnlyTrue true
+        | _ -> return FsCheck.Prop.discard ()
+    }
+
+type MyArbs =
+  static member OnlyTrue () =  Arb.fromGen chooseTrueGen
+
+[<Property(Arbitrary = [| typeof<MyArbs> |])>]
+let testOptimizedProperty (OnlyTrue v) =
+    v
